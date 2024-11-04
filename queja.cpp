df5272ebdd36e8f95fd98079f3aab2291c6cac13
lucas535f1 <lucasmarsigliaf@gmail.com>
2024-11-04 20:11:55 -0300

    primer modulo

--- queja.cpp
+++ queja.cpp
@@ -1,14 +1,44 @@
 #include "../include/queja.h"
 
-struct rep_queja { };
+struct rep_queja
+{
+  TFecha fecha;
+  TProducto producto;
+  TCliente cliente;
+  char comentario[MAX_COMENTARIO];
+};
 
 TQueja crearTQueja(TFecha fecha, TProducto producto, TCliente cliente,
-                   const char comentario[MAX_COMENTARIO]) {
-  return NULL;
+                   const char comentario[MAX_COMENTARIO])
+{
+  TQueja queja = new rep_queja;
+  strcpy(queja->comentario, comentario);
+  queja->fecha = fecha;
+  queja->cliente = cliente;
+  queja->producto=producto;
+  return queja;
 }
 
-void imprimirTQueja(TQueja queja) { }
+void imprimirTQueja(TQueja queja)
+{
+  printf("Fecha: ");
+  imprimirTFecha(queja->fecha);
+  printf("\nCliente: ");
+  imprimirTCliente(queja->cliente);
+  imprimirTProducto(queja->producto);
+  printf("\nComentario: %s\n", queja->comentario);
+}
 
-void liberarTQueja(TQueja &queja) { }
+void liberarTQueja(TQueja &queja)
+{
+  liberarTFecha(queja->fecha);
+  liberarTCliente(queja->cliente);
+  liberarTProducto(queja->producto);
+  delete queja;
+  queja = nullptr;
+}
 
-TFecha fechaTQueja(TQueja queja) { return NULL; }
+TFecha fechaTQueja(TQueja queja)
+{
+  return queja->fecha;
+}
