1683e8806e80e3342b3582672d7ad7a6b1fe1ea9
lucas535f1 <lucasmarsigliaf@gmail.com>
2024-11-04 20:55:12 -0300

    modulo 4.2 bien

--- tablaQuejas.cpp
+++ tablaQuejas.cpp
@@ -1,25 +1,93 @@
 #include "../include/tablaQuejas.h"
 
-struct rep_tablaQuejas { };
+struct rep_nodoQueja
+{
+  TQueja queja;
+  rep_nodoQueja *sig;
+};
 
-int funcionHash(TFecha fecha, int cantEstimadas){
-    return (31 * (int) mesTFecha(fecha) + (int) diaTFecha(fecha)) % cantEstimadas;
+struct rep_tablaQuejas
+{
+  rep_nodoQueja **quejas;
+  int total;
+  int cota;
+};
+
+int funcionHash(TFecha fecha, int cantEstimadas)
+{
+  return (31 * (int)mesTFecha(fecha) + (int)diaTFecha(fecha)) % cantEstimadas;
 }
 
-TablaQuejas crearTablaQuejas(int cantEstimadas) { 
-  return NULL;
+TablaQuejas crearTablaQuejas(int cantEstimadas)
+{
+  TablaQuejas tabla = new rep_tablaQuejas;
+  tabla->total = 0;
+  tabla->cota = cantEstimadas;
+  tabla->quejas = new rep_nodoQueja *[cantEstimadas];
+  for (int i = 0; i < cantEstimadas; i++)
+  {
+    tabla->quejas[i] = nullptr;
+  };
+  return tabla;
 }
 
-void agregarQuejaTablaQuejas(TablaQuejas tabla, TQueja queja) { }
+void agregarQuejaTablaQuejas(TablaQuejas tabla, TQueja queja)
+{
+  rep_nodoQueja *nodo = new rep_nodoQueja;
+  nodo->queja = queja;
+  int index = funcionHash(fechaTQueja(queja), tabla->cota);
+  nodo->sig = tabla->quejas[index];
+  tabla->quejas[index] = nodo;
+  tabla->total++;
+}
 
-void imprimirTablaQuejas(TablaQuejas tabla) { }
+void imprimirTablaQuejas(TablaQuejas tabla)
+{
+  for (int i = 0; i < tabla->cota; i++)
+  {
+    rep_nodoQueja *quejas = tabla->quejas[i];
 
-bool perteneceQuejaTablaQuejas(TablaQuejas tabla, TFecha fecha) {
+    if (quejas == nullptr)
+    {
+      printf("No hay elementos guardados en la posicion %d de la tabla.\n", i);
+    }
+    else
+    {
+      printf("Elementos en la posicion %d de la tabla:\n", i);
+      while (quejas != nullptr)
+      {
+        imprimirTQueja(quejas->queja);
+        quejas = quejas->sig;
+      }
+    }
+  }
+}
+
+bool perteneceQuejaTablaQuejas(TablaQuejas tabla, TFecha fecha)
+{
   return false;
 }
 
-TQueja obtenerQuejaTablaQuejas(TablaQuejas tabla, TFecha fecha) {
+TQueja obtenerQuejaTablaQuejas(TablaQuejas tabla, TFecha fecha)
+{
   return NULL;
 }
 
-void liberarTablaQuejas(TablaQuejas &tabla) {}
+void liberarTablaQuejas(TablaQuejas &tabla)
+{
+  for (int i = 0; i < tabla->cota; i++)
+  {
+    rep_nodoQueja *quejas = tabla->quejas[i];
+
+    while (quejas != nullptr)
+    {
+      rep_nodoQueja* aux = quejas;
+      quejas = quejas->sig;
+      liberarTQueja(aux->queja);
+      delete aux;
+    }
+  }
+  delete[] tabla->quejas;
+  delete tabla;
+  tabla=nullptr;
+}
