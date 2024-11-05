a1f0679f5c5f46455351436f60c8740107e7f4f9
lucas535f1 <lucasmarsigliaf@gmail.com>
2024-11-05 10:28:02 -0300

    Cola envios comenzado

--- colaEnvios.cpp
+++ colaEnvios.cpp
@@ -1,32 +1,56 @@
 #include "../include/colaEnvios.h"
 
-struct rep_cola_envios {
+struct rep_cola_envios
+{
+    TEnvio *cola;
+    int cantidad;
+    int cota;
 };
 
-TColaEnvios crearTColaEnvios(int N) { 
+TColaEnvios crearTColaEnvios(int N)
+{
+    TColaEnvios nuevo = new rep_cola_envios;
+    nuevo->cantidad = 0;
+    nuevo->cota = N;
+    nuevo->cola = new rep_envio *[N];
+    for (int i = 0; i < N; i++)
+    {
+        nuevo->cola[i] = nullptr;
+    };
     return NULL;
- }
+}
 
-void encolarEnvioTColaEnvios(TColaEnvios &colaEnvios, TEnvio envio) {
-    
+void encolarEnvioTColaEnvios(TColaEnvios &colaEnvios, TEnvio envio)
+{
+
 }
 
 int cantidadTColaEnvios(TColaEnvios colaEnvios) { return 0; }
 
-void imprimirTColaEnvios(TColaEnvios colaEnvios) {
+void imprimirTColaEnvios(TColaEnvios colaEnvios)
+{
 }
 
-TEnvio desencolarTColaEnvios(TColaEnvios &colaEnvios) { 
+TEnvio desencolarTColaEnvios(TColaEnvios &colaEnvios)
+{
     return NULL;
-
- }
-
-void liberarTColaEnvios(TColaEnvios &colaEnvios) {
-  
 }
 
-void invertirPrioridadTColaEnvios(TColaEnvios &colaEnvio) {
-  
+void liberarTColaEnvios(TColaEnvios &colaEnvios)
+{
+    for (int i = 0; i < colaEnvios->cota; i++)
+    {
+        if(colaEnvios->cola[i]!=nullptr){
+            liberarTEnvio(colaEnvios->cola[i]);
+        }
+    };
+    delete[] colaEnvios->cola;
+    delete colaEnvios;
+    colaEnvios=nullptr;
+}
+
+void invertirPrioridadTColaEnvios(TColaEnvios &colaEnvio)
+{
 }
 
 TEnvio masPrioritarioTColaEnvios(TColaEnvios colaEnvio) { return NULL; }
